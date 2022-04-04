3c09d88b4452ab42e55931e83d614c71e290ce2b
Vlad-Elkin <elkin.vladislav8@gmail.com>
2022-04-04 11:08:29 +0300

    Upload 1

--- app/src/main/java/com/example/practice_3_5/MainActivity.kt
+++ app/src/main/java/com/example/practice_3_5/MainActivity.kt
@@ -1,36 +1,33 @@
 package com.example.practice_3_5
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.FrameLayout
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var swap:Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val caption:Button = findViewById(R.id.caption)
-        supportFragmentManager.beginTransaction().add(R.id.fragment1,RedFragment()).commit()
-        supportFragmentManager.beginTransaction().add(R.id.fragment2,BlueFragment()).commit()
-        var swap = false
-        caption.setOnClickListener {
-            val fragment1:Fragment
-            val fragment2:Fragment
+        if (savedInstanceState==null){
+            supportFragmentManager.beginTransaction().add(R.id.fragment1,RedFragment()).commit()
+            supportFragmentManager.beginTransaction().add(R.id.fragment2,BlueFragment()).commit()
+        }
+
+        findViewById<Button>(R.id.caption).setOnClickListener{
             if (swap){
-                fragment1=RedFragment()
-                fragment2=BlueFragment()
-                swap = false
-            }else{
-                fragment1=BlueFragment()
-                fragment2=RedFragment()
-                swap = true
+                supportFragmentManager.beginTransaction().replace(R.id.fragment1,BlueFragment()).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment2,RedFragment()).commit()
+
+                Log.d("swap1",swap.toString())
             }
-            supportFragmentManager.beginTransaction().replace(R.id.fragment1,fragment1).commit()
-            supportFragmentManager.beginTransaction().replace(R.id.fragment2,fragment2).commit()
+            else{
+                supportFragmentManager.beginTransaction().replace(R.id.fragment2,BlueFragment()).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.fragment1,RedFragment()).commit()
+                Log.d("swap2",swap.toString())
+            }
+            swap!=swap
         }
     }
 }
